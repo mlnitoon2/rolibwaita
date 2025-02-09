1e461332a4685d2bf350881b956808e872c4ece5
eyesing <77251330+mlnitoon2@users.noreply.github.com>
2025-02-09 01:46:01 -0600

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -14,7 +14,7 @@
 local rolibwaita = {}
 
 -- \\ Variables // --
-local Assets = game:GetObjects("rbxassetid://96186871004807")[1]
+local Assets = game:GetObjects("rbxassetid://101695147467873")[1]
 
 local UIS = game:GetService("UserInputService")
 local TweenService = game:GetService("TweenService")
